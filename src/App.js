1c5e6f1578c3a53f1834b9c6fd5ae25f9d08368b
Robert Stolz <rstolz@ucdavis.edu>
2020-10-13 09:31:24 -0700

    v2

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import axios from 'axios';
 import { orderBy } from 'lodash';
 
 function App() {
+  const [selected_trade, set_selected_trade] = React.useState()
   const [current_trades, set_current_trades] = React.useState()
   React.useEffect(() => {
-    async function pull_data() {
-      const result = await axios.get("https://esi.evetech.net/latest/markets/10000001/orders/")
-      const sorted_result = orderBy(result.data,(t) => t.price, ["desc"])
-      set_current_trades(sorted_result)
-      console.log(result.data.length)
-    }
     pull_data()
   }, [])
+
+  async function pull_data() {
+    const result = await axios.get("http://ec2-54-215-48-242.us-west-1.compute.amazonaws.com:5000/api/trades")
+    const sorted_result = orderBy(result.data,(t) => t.ppj, ["desc"])
+    set_current_trades(sorted_result)
+  }
+
+  const select_trade=(trade_id) => () => {
+    set_selected_trade(trade_id)
+  }
+
+  function accept_selected_trade() {
+    axios.post("http://ec2-54-215-48-242.us-west-1.compute.amazonaws.com:5000/api/accept-trade", {selected_trade: selected_trade.id})
+  }
+
   return (
     <div className="App">
       <Grid container>
@@ -33,49 +43,52 @@ function App() {
           <Button variant="contained" style={{margin: '8px', backgroundColor: 'lightblue'}}>
             Filter Trades
           </Button>
+        {selected_trade && (
+          <Trade 
+          trade = {selected_trade}/>
+        )}
         </Grid>
         <Grid item>
           <List style={{maxHeight: '480px', overflow: "auto"}}>
           {current_trades && current_trades.map((t) => {
             return(
-              <ListItem style={{border: '1px solid black'}}>
+              <ListItem key={t.id} style={{border: '1px solid black'}} onClick={select_trade(t)}>
                 <Trade 
-                key={t.order_id}
-                duration={t.duration}
-                price={t.price}
-                location_id={t.location_id}
-                min_volume={t.min_volume}/>
+                trade = {t}
+                is_selected={selected_trade?.id === t.id}/>
               </ListItem>
             )
           })}
           </List>
-          <Button variant="contained" style={{margin: '8px', backgroundColor: 'lightgreen'}}>
+          <Button onClick={accept_selected_trade} variant="contained" style={{margin: '8px', backgroundColor: 'lightgreen'}}>
             Accept
           </Button>
           <Button variant="contained" style={{margin: '8px', backgroundColor: 'red'}}>
             Abandon
           </Button>
-
+          <Button onClick={pull_data} variant="contained" style={{margin: '8px', backgroundColor: 'lightblue'}}>
+            Refresh Trades
+          </Button>
         </Grid>
       </Grid>
     </div>
   );
 }
 
-function Trade({duration, price ,location_id, min_volume}) {
+function Trade({trade, is_selected}) {
   return (
-    <Grid container>
+    <Grid container style={{backgroundColor: is_selected?'blueviolet': undefined}}>
       <Grid item xs={6}>
-        {duration}
+        {`${trade.source} -> ${trade.destination}`}
       </Grid>
       <Grid item xs={6}>
-        {price}
+        {trade.total_value} isk
       </Grid>
       <Grid item xs={6}>
-        {location_id}
+        {trade.item_name}
       </Grid>
       <Grid item xs={6}>
-        {min_volume}
+        {trade.ppj} isk/jump
       </Grid>
     </Grid>
   );
